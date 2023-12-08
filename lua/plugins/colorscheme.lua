f29e8be543ec5c02a016b4ee3878281ac09e343a
Ryan Olson <ryolson@me.com>
2023-12-07 22:52:02 -0700

    Fix indent guide

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -2,4 +2,8 @@ return {
   "maxmx03/solarized.nvim",
   lazy = true,
   priority = 1000,
+  config = function()
+    vim.cmd([[highlight IblIndent guifg=#2B4D57]])
+    vim.cmd([[highlight IblScope guifg=#2B4D57]])
+  end,
 }
